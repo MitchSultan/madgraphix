76f1af0ebd54b7d6e814b7b33f09d06ffa3539f2
MitchSultan <admin@mitch.sultan.theotherguyske.org>
2025-11-26 10:16:31 +0300

    pleease

--- app/instruments/page.js
+++ app/instruments/page.js
@@ -1,7 +1,22 @@
-import { createClient } from '/utils/supabase/server';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
 
 export default async function Instruments() {
-  const supabase = await createClient();
+  const cookieStore = await cookies();
+  
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
+  
   const { data: instruments } = await supabase.from("instruments").select();
 
   return <pre>{JSON.stringify(instruments, null, 2)}</pre>
